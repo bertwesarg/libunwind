d3f130c8cbeaea2e8acb08723b408de4148b0543
Bert Wesarg <bert.wesarg@tu-dresden.de>
2017-01-26 16:22:15 +0100

    Use the `elf_w()` macro everywhere, when calling into arch-dep ELF functions.

--- src/ptrace/_UPT_get_proc_name.c
+++ src/ptrace/_UPT_get_proc_name.c
@@ -32,10 +32,8 @@ _UPT_get_proc_name (unw_addr_space_t as, unw_word_t ip,
 {
   struct UPT_info *ui = arg;
 
-#if ELF_CLASS == ELFCLASS64
-  return _Uelf64_get_proc_name (as, ui->pid, ip, buf, buf_len, offp);
-#elif ELF_CLASS == ELFCLASS32
-  return _Uelf32_get_proc_name (as, ui->pid, ip, buf, buf_len, offp);
+#ifdef ELF_CLASS
+  return elf_w (get_proc_name) (as, ui->pid, ip, buf, buf_len, offp);
 #else
   return -UNW_ENOINFO;
 #endif
